26a0ab8301cde18602155dad74f4e6d48774ef44
Cristine Sato <cristine.sato@gmail.com>
2020-05-20 14:28:30 +0000

    PrintVariables.java edited online with Bitbucket

--- PrintVariables.java
+++ PrintVariables.java
@@ -4,6 +4,7 @@ class PrintVariables {
     	Double number = -10.6;
     	
     	System.out.println("I am " + "awesome.");
+        System.out.println("Today is " + "great day.");
     	System.out.println("Number = " + number);
     }
 }
