66f4399adda4bc9d208137de9993246998ae8d54
cristinesato <cristine.sato@gmail.com>
2020-05-20 14:54:15 +0000

    add Cars.java

---
+++ Cars.java
@@ -0,0 +1,12 @@
+class PrintVariables {
+	    public static void main(String[] args) {
+		        	
+		        	Double number = -10.6;
+				    	
+				    	System.out.println("I am " + "awesome.");
+					        System.out.println("Today is " + "great day.");
+						        System.out.println("Today is " + "Wednesday.");
+							    	System.out.println("Number = " + number);
+								    }
+}
+
