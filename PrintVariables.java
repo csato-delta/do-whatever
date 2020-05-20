ab117db8eb70fdb57b3eea6e094fa4f232fdd7b5
Cristine Sato <cristine.sato@gmail.com>
2020-05-20 14:46:24 +0000

    PrintVariables.java edited online with Bitbucket

--- PrintVariables.java
+++ PrintVariables.java
@@ -5,6 +5,7 @@ class PrintVariables {
     	
     	System.out.println("I am " + "awesome.");
         System.out.println("Today is " + "great day.");
+        System.out.println("Today is " + "Wednesday.");
     	System.out.println("Number = " + number);
     }
 }
