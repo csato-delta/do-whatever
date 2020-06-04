3a67b26fbd6a7ac803e36e67350eab59a09c69e2
Cristine <63407792+csato-delta@users.noreply.github.com>
2020-06-03 22:27:06 -0300

    add new stuff
    test

--- PrintVariables.java
+++ PrintVariables.java
@@ -6,6 +6,7 @@ class PrintVariables {
     	System.out.println("I am " + "awesome.");
         System.out.println("Today is " + "great day.");
         System.out.println("Today is " + "Wednesday.");
-    	System.out.println("Number = " + number);
+    	System.out.println("Number = " + number); 
+        System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkk" ); 
     }
 }
