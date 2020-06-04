1c3b53e4b464555fef351ed7de8811f2d900f73b
Cristine <63407792+csato-delta@users.noreply.github.com>
2020-06-04 14:31:28 -0300

    pr (*/from)
    pr (*/from)

--- Houses.java
+++ Houses.java
@@ -4,7 +4,7 @@ class Cars {
 		System.out.println("My house id" + car);
 		System.out.println("Today is " + "great day.");
 		System.out.println("Today is " + "Wednesday.");
-		System.out.println("Tomorrow is " + "Another Day.");
-		System.out.println("Tomorrow is " + "Another Day Indeed.");
+	//	System.out.println("Tomorrow is " + "Another Day.");
+	//	System.out.println("Tomorrow is " + "Another Day Indeed.");
 }
 }
