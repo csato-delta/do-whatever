d63d91cd50eb46f45022f5d36bacced44805c9ed
Cristine <63407792+csato-delta@users.noreply.github.com>
2020-06-04 13:53:24 -0300

    WIP test
    pr for today

--- Cars.java
+++ Cars.java
@@ -4,8 +4,8 @@ class Cars {
 		System.out.println("My car is" + car);
 		System.out.println("Today is " + "great day.");
 		System.out.println("Today is " + "Wednesday.");
-		System.out.println("Tomorrow is " + "Another Day.");
-		System.out.println("Tomorrow is " + "Another Day Indeed.");
+	//	System.out.println("Tomorrow is " + "Another Day.");
+	//	System.out.println("Tomorrow is " + "Another Day Indeed.");
 }
 }
 
